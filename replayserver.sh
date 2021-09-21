e660cfa020c636b163e142bf0a03ead99bae12aa
Axle1975 <loweam@gmail.com>
2021-09-21 22:27:29 +1000

    line endings

--- replayserver.sh
+++ replayserver.sh
@@ -5,7 +5,6 @@ echo "RS_PORT=${RS_PORT}"
 echo "RS_DELAY_SECS=${RS_DELAY_SECS}"
 echo "RS_MAX_SEND_RATE=${RS_MAX_SEND_RATE}"
 echo "RS_LOBBY_SERVER=${RS_LOBBY_SERVER}"
-echo "RS_USERNAME=${RS_LOBBY_USERNAME}"
-echo "len(RS_PASSWORD)=${#RS_PASSWORD}"
+echo "RS_USERNAME=${RS_USERNAME}"
 echo ./replayserver --loglevel ${RS_LOG_LEVEL} --demofile "${RS_DEMO_FILE_TEMPLATE}" --port ${RS_PORT} --livedelaysecs ${RS_DELAY_SECS} --maxsendrate ${RS_MAX_SEND_RATE} --lobbyserver ${RS_LOBBY_SERVER} --replayer 1
 ./replayserver --loglevel ${RS_LOG_LEVEL} --demofile "${RS_DEMO_FILE_TEMPLATE}" --port ${RS_PORT} --livedelaysecs ${RS_DELAY_SECS} --maxsendrate ${RS_MAX_SEND_RATE} --lobbyserver ${RS_LOBBY_SERVER} --replayer 1
